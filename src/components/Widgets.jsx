eb0d7f6b519a6ddbfb5ab609eb6da552868caeda
Johnny <johnny.martens@protonmail.com>
2022-10-05 21:51:03 -0400

    updated widgets section

--- src/components/Widgets.jsx
+++ src/components/Widgets.jsx
@@ -1,10 +1,32 @@
+import { Search } from "@mui/icons-material";
 import React from "react";
+import {
+  TwitterShareButton,
+  TwitterTimelineEmbed,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
 import "./Widgets.css";
 
 function Widgets() {
   return (
     <div className="widgets">
-      <h2>Widgets</h2>
+      <div className="widgets__input">
+        <Search className="widgets__search-icon" />
+        <input placeholder="Search Twitter" type="text" />
+      </div>
+      <div className="widgets__widget-container">
+        <h2>What's Happening</h2>
+        <TwitterTweetEmbed tweetId={"858551177860055040"} />
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="cleverqazi"
+          options={{ height: 400 }}
+        />
+        <TwitterShareButton
+          url={"https://facebook.com/cleverprogrammer"}
+          options={{ text: "#reactjs is awesome", via: "cleverqazi" }}
+        />
+      </div>
     </div>
   );
 }
